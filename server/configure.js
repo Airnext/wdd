cf085792ffdfa5b9bc79b3f4fbb7ede579ee2bc3
airnext <ernestium2009@yahoo.com>
2019-03-31 02:41:19 +0100

    commit all changes

--- server/configure.js
+++ server/configure.js
@@ -5,12 +5,16 @@ let exphbs = require('express-handlebars');
 let	favicon = require('serve-favicon');
 let	bodyParser = require('body-parser');
 let expressValidator = require('express-validator');
+let multer = require('multer');
+let flash = require('express-flash');
 
 module.exports = function(app){
 	app.use(favicon(path.join(__dirname, '../public', 'img', 'favicon.png')));
 	app.use(bodyParser.urlencoded({extended:false}));
 	app.use(bodyParser.json());
 	app.use(expressValidator());
+	app.use(multer({'dest':'./public/upload/temp'}).single('file'));
+	app.use(flash());
 	app.use('/public/', express.static(path.join(__dirname, '../public')));
 	routes(app);
 	app.engine('handlebars', exphbs.create().engine);
